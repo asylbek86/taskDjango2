3d0b9b6c67a75625b67f0b4077ad61d3a93fa79d
asylbek86 <alibekgan1986@gmail.com>
2024-10-03 21:32:00 +0600

    lesson

--- car_shop/urls.py
+++ car_shop/urls.py
@@ -4,10 +4,10 @@ from . import views
 
 
 urlpatterns = [
-    path('cars/', views.car_list_view, name="cars"),
-    path('cars/<int:id>/', views.car_detail_view, name="car_detail"),
-    path('car_create/', views.car_create_view, name="car_create"),
-    path('cars/<int:id>/update/', views.update_object_view, name="car_update"),
-    path('cars/<int:id>/delete/', views.delete_object_view, name="car_delete"),
+    path('cars/', views.CarListView.as_view(), name="cars"),
+    path('cars/<int:id>/', views.CarDetailView.as_view(), name="cars_detail"),
+    path('car_create/', views.CreateCarView.as_view(), name="car_create"),
+    path('cars/<int:id>/update/', views.CarUpdateView.as_view(), name="car_update"),
+    path('cars/<int:id>/delete/', views.CarDeleteView.as_view(), name="car_delete"),
     path('cars/<int:id>/comment/', views.CarCommentView.as_view(), name="car_comment"),
 ]
--- car_shop/views.py
+++ car_shop/views.py
@@ -1,9 +1,115 @@
 from django.shortcuts import render, get_object_or_404
-from django.views.generic import CreateView
+from django.views.generic import CreateView, ListView, DetailView, UpdateView, DeleteView
 
 from .models import Car, CommentCar
 from .forms import CarForm, CommentFrom
 from django.http import HttpResponse
+
+
+#**************************************************************************
+# предварительный просмотр
+
+class CarListView(ListView):
+    template_name = 'car_list.html'
+    queryset = Car.objects.all()
+
+    def get_queryset(self):
+        return Car.objects.all()
+
+# def car_list_view(request):
+#     cars = Car.objects.all()
+#     return render(request, 'car_list.html', {'cars': cars})
+
+# детальный просмотр
+
+
+class CarDetailView(DetailView):
+    template_name = 'car_detail.html'
+
+    def get_object(self, **kwargs):
+        car_id = self.kwargs.get('id')
+        return get_object_or_404(Car, id=car_id)
+
+# def car_detail_view(request, id):
+#     car_detail = get_object_or_404(Car, id=id)
+#     return render(request, 'car_detail.html', {'car_detail': car_detail})
+
+
+# добавление машины
+
+
+class CreateCarView(CreateView):
+    template_name = 'car_create.html'
+    form_class = CarForm
+    queryset = Car.objects.all()
+    success_url = '/cars/'
+
+    def form_valid(self, form):
+        print(form.cleaned_data)
+        return super(CreateCarView, self).form_valid(form=form)
+
+
+
+# def car_create_view(request):
+#     method = request.method
+#     if method == 'POST':
+#         form = CarForm(request.POST, request.FILES)
+#         if form.is_valid():
+#             form.save()
+#             return HttpResponse('<h2>Машина успешно добавленно</h2>')
+#     else:
+#         form = CarForm()
+#     return render(request, 'car_create.html', {'form': form})
+
+# изменение и обновление машин
+
+class CarUpdateView(UpdateView):
+    template_name = 'update_car.html'
+    form_class = CarForm
+    success_url = '/cars/'
+
+    def get_object(self, **kwargs):
+        car_id = self.kwargs.get('id')
+        return get_object_or_404(Car, id=car_id)
+
+    def form_valid(self, form):
+        return super(CarUpdateView, self).form_valid(form=form)
+
+
+
+
+# def update_object_view(request, id):
+#     car_object = get_object_or_404(Car, id=id)
+#     if request.method == 'POST':
+#         form = CarForm(instance=car_object, data=request.POST)
+#         if form.is_valid():
+#             form.save()
+#             return HttpResponse('<h1>Машина успешно обновлено</h1>')
+#     else:
+#         form = CarForm(instance=car_object)
+#         return render(request, 'update_car.html', {'form': form,
+#                                                                         'object': car_object
+#                                                                         })
+
+
+# Удаление машины
+
+class CarDeleteView(DeleteView):
+    template_name = 'car_delete.html'
+    success_url = '/cars/'
+
+    def get_object(self, **kwargs):
+        car_id = self.kwargs.get('id')
+        return get_object_or_404(Car, id=car_id)
+
+
+
+
+# def delete_object_view(request, id):
+#     car_object = get_object_or_404(Car, id=id)
+#     car_object.delete()
+#     return HttpResponse('<h1>Машина успешно удалено</h1>')
+
 
 
 
@@ -12,8 +118,10 @@ from django.http import HttpResponse
 class CarCommentView(CreateView):
     template_name = 'car_comment.html'
     form_class = CommentFrom
-    queryset = Car.objects.all()
+    queryset = CommentCar.objects.all()
     success_url = '/cars/'
+
+
     def get_object(self, **kwargs):
         car_id = self.kwargs.get('id')
         return get_object_or_404(CommentCar, id=car_id)
@@ -22,62 +130,6 @@ class CarCommentView(CreateView):
     def form_valid(self, form):
         print(form.clean)
         return super(CarCommentView, self).form_valid(form=form)
-
-
-#**************************************************************************
-
-def car_list_view(request):
-    cars = Car.objects.all()
-    return render(request, 'car_list.html', {'cars': cars})
-
-
-
-def car_detail_view(request, id):
-    car_detail = get_object_or_404(Car, id=id)
-    return render(request, 'car_detail.html', {'car_detail': car_detail})
-
-
-# добавление машины
-
-def car_create_view(request):
-    method = request.method
-    if method == 'POST':
-        form = CarForm(request.POST, request.FILES)
-        if form.is_valid():
-            form.save()
-            return HttpResponse('<h2>Машина успешно добавленно</h2>')
-    else:
-        form = CarForm()
-    return render(request, 'car_create.html', {'form': form})
-
-# изменение и обновление машин
-
-def update_object_view(request, id):
-    car_object = get_object_or_404(Car, id=id)
-    if request.method == 'POST':
-        form = CarForm(instance=car_object, data=request.POST)
-        if form.is_valid():
-            form.save()
-            return HttpResponse('<h1>Машина успешно обновлено</h1>')
-    else:
-        form = CarForm(instance=car_object)
-        return render(request, 'update_car.html', {'form': form,
-                                                                        'object': car_object
-                                                                        })
-
-
-# Удаление машины
-
-
-def delete_object_view(request, id):
-    car_object = get_object_or_404(Car, id=id)
-    car_object.delete()
-    return HttpResponse('<h1>Машина успешно удалено</h1>')
-
-
-
-
-
 
 
 
